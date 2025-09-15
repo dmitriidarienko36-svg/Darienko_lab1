4e87a8f2801ce7127d20afb0ae2e45dc7bd4a4e0
dmitriidarienko36-svg <dmitriidarienko36@gmail.com>
2025-09-15 20:20:01 +0300

    Бесконечное меню и две структуры
    Создано две структуры (Pipe - структура трубы, KS - структура компрессорной станции) и меню, запрашивающее в бесконечном цикле действие от пользователя.

---
+++ lab1/lab1.cpp
@@ -0,0 +1,78 @@
+﻿// lab1.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+struct Pipe {
+    string name;
+    float length;
+    int diameter;
+    bool condition;
+};
+
+struct KS {
+    string name = "";
+    int count;
+    int inwork;
+    string category;
+};
+
+void menu() {
+    while (true)
+    {
+        switch (number) {
+        case 1:
+        {
+
+            break
+        }
+        case 2:
+        {
+
+            break
+        }
+        case 3:
+        {
+
+            break
+        }
+        case 4:
+        {
+
+            break
+        }
+        case 5:
+        {
+
+            break
+        }
+        case 6:
+        {
+
+            break
+        }
+        case 7:
+        {
+
+            break
+        }
+        case 0:
+        {
+
+            break
+        }
+        }
+    }
+}
+
+int main()
+{
+    std::cout << "Hello World!\n";
+    Pipe pipe1;
+    KS ks1;
+    menu();
+    return 0;
+}
