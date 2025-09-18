32feff414b1a62a3d1ddbcb5a89dcf51b1acb4b2
dmitriidarienko36-svg <dmitriidarienko36@gmail.com>
2025-09-18 22:17:14 +0300

    Обновление по данным семинара
    Добавление трубы, проверка на double, показ трубы и улучшение меню

--- lab1/lab1.cpp
+++ lab1/lab1.cpp
@@ -7,36 +7,67 @@
 using namespace std;
 
 struct Pipe {
-    string name;
+    string name="";
     float length;
     int diameter;
-    bool condition;
+    bool status;
 };
 
-struct KS {
+struct CS {
     string name = "";
     int count;
     int inwork;
     string category;
 };
 
-void menu() {
-    while (true)
-    {
-        switch (number) {
+void AddPipe(Pipe& t) {
+    cout << "Insert pipe name: ";
+    getline(cin, t.name);
+    cout << "Insert pipe length: ";
+    CheckDoubleInput(t.length);
+}
+
+void CheckDoubleInput(double length) {
+    while (true) {
+        cin >> length;
+        if (cin.fail() || length <= 0) {
+            cin.clear();
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+            cout << "Insert positive number!" << endl;
+        }
+        else {
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+            return length;
+        }
+    }
+}
+
+void ShowPipe(Pipe& t) {
+    cout << "Название: " << t.name << endl;
+    cout << "Длина: " << t.length << endl;
+    cout << "Диаметр: " << t.diameter << endl;
+    cout << "В ремонте: " << (t.status? "Да" : "Нет") << endl;
+}
+void menu(Pipe& t, CS& cs) {
+    int option;
+    while (true){
+        cout << "Выберите действие: \n 1. Добавить трубу; \n 2.Добавить КС; \n 3.Просмотр всех объектов; \n 4.Редактировать трубу; \n 5.Редактировать КС; \n 6.Сохранить; \n 7.Загрузить; \n 0.Выход;";
+        cin >> option;
+        switch (option) {
         case 1:
         {
-
+            AddPipe(t)
             break
         }
         case 2:
         {
-
+            
             break
         }
         case 3:
         {
-
+            ShowPipe()
+            
             break
         }
         case 4:
@@ -71,8 +102,8 @@ void menu() {
 int main()
 {
     std::cout << "Hello World!\n";
-    Pipe pipe1;
-    KS ks1;
-    menu();
+    Pipe pipe;
+    CS cs;
+    menu(pipe,cs);
     return 0;
 }
